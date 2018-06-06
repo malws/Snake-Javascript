a9120b6932ab5dd413974fa37389ac5e037880f3
Malwina Ostaszewska <m.ostaszewska13@gmail.com>
2018-06-06 18:02:34 +0200

    added snake object

--- js/game.js
+++ js/game.js
@@ -22,6 +22,14 @@ snakeImage.onload = function () {
 };
 snakeImage.src = "images/snake.png";
 
+// Game objects
+var snake = {
+	"x":0,
+	"y":0,
+	"direction": 0,
+	"speed": 200
+}
+
 var board = new Array(20);
 for (var i=0; i <20; i++)
     board[i] = new Array(20);
@@ -31,13 +39,75 @@ for (var i=0; i < 20; i++) {
 		}
 }
 
-function update () {	
+// Handle keyboard controls
+var keysDown = {};
+
+addEventListener("keydown", function (e) {
+	if (e.keyCode == 37) { // Player press left
+		if(snake.direction > 0) snake.direction--;
+		else snake.direction = 3;
+	 }
+	 if (e.keyCode == 39) { // Player press right
+		if(snake.direction < 3) snake.direction++;
+		else snake.direction = 0;
+	 }
+}, false);
+
+
+// Functions
+function setSnake() {
+	board[10][0] = -1;
+	snake.x = 10;
+	snake.y = 0;
+};
+
+function update () {
+	var foundHead = false;
+	for (var i=0; i < 20; i++) {
+		for(var j = 0; j < 20; j++) {
+			// Head update
+			if(board[i][j] == -1  && !foundHead) {
+				foundHead = true;
+				switch (checkNext()) {
+					case 1: // Snake finds apple
+					break;
+					case 0:
+					board[snake.x][snake.y] = -1;
+					board[i][j] = 0;
+					break;					
+					case -1:
+					break;
+				}
+			}
+		}
+	}	
+}
+
+function checkNext() {
+	switch (snake.direction) {
+		case 1: snake.x--; break;
+        case 3: snake.x++; break;
+        case 2: snake.y--; break;
+        case 0: snake.y++; break;
+	}
+	if((snake.x == -1 || snake.y == -1) || (snake.x > 19 || snake.y > 19)) return -1;	
+	else if (board[snake.x][snake.y] == -2) return 1;
+	else return 0;
 }
 
 function render () {
 	if (bgReady) {
 		ctx.drawImage(bgImage, 0, 0);
 	}
+	for (var i=0; i <20; i++) {
+		for(var j = 0; j < 20; j++) {
+			if(board[i][j] == -1 || board[i][j] > 0) {
+				if (snakeReady) {
+					ctx.drawImage(snakeImage, i * 20, j * 20);
+				}
+			}
+		}
+	}	
 };
 
 var main = function () {
@@ -46,5 +116,6 @@ var main = function () {
 };
 
 (function init() {
+	setSnake();
 	var loop = setInterval(main, 200);
 })();
